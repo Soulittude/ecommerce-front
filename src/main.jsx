fec4665557fb8b5b93f9d73d15dedcfa641f5f30
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-21 04:40:32 +0300

    q

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import App from "./App.jsx";
 
 // Redux store
 import { Provider } from "react-redux";
-import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store";
 
 // React Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -26,9 +27,11 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </PersistGate>
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cartSlice";
 import orderReducer from "./orderSlice";
 import authReducer from "./authSlice";
 
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+};
+
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistReducer(cartPersistConfig, cartReducer),
     order: orderReducer,
     auth: authReducer,
   },
-  // middleware: getDefaultMiddleware => getDefaultMiddleware().concat(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
